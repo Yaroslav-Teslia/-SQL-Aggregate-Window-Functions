8df1652ff88959e4490bf83b512df5af0028c8b0
Yaroslav-Teslia <yaroslav.teslia@gmail.com>
2024-11-20 16:08:48 +0200

    Update sql_win_func.sql

--- sql_win_func.sql
+++ sql_win_func.sql
@@ -1,84 +1,99 @@
--- 1. Підрахунок фільмів за роками
+-- 1. Визначення значення на наступному рядку (LEAD) для рейтингу IMDB у межах року
 SELECT "Movie Name",
        "Release Year",
-       COUNT(*) OVER (PARTITION BY "Release Year") AS nmb_in_the_year
-FROM movies
-ORDER BY nmb_in_the_year DESC;
+       "IMDB Rating",
+       LEAD("IMDB Rating") OVER (PARTITION BY "Release Year") AS next_imdb_rating
+FROM movies;
 
--- 2. Середня тривалість фільмів за жанром
+-- 2. Визначення значення на попередньому рядку (LAG) для тривалості фільмів у межах жанру
 SELECT "Movie Name",
-       ROUND(AVG("Duration") OVER (PARTITION BY "Genre"), 0) AS avg_duration_by_genre
+       "Genre",
+       "Duration",
+       LAG("Duration") OVER (PARTITION BY "Genre") AS prev_duration
 FROM movies;
 
--- 3. Максимальний рейтинг IMDB у кожному жанрі
-SELECT *,
-       MAX("IMDB Rating") OVER (PARTITION BY "Genre") AS max_rating_by_genre
-FROM movies;
-
--- 4. Касові збори в жанрі
+-- 3. Пошук першого рейтингу IMDB для кожного жанру
 WITH cte AS (
-    SELECT *, 
-           CAST(REPLACE(REPLACE(REPLACE("Gross", '$', ''), 'M', ''), '.', '') AS INTEGER) AS gross_clean
+    SELECT *,
+           FIRST_VALUE("IMDB Rating") OVER (PARTITION BY "Genre") AS first_movie
     FROM movies
-    WHERE "Gross" != 'NA'
 )
-SELECT *,
-       SUM(gross_clean) OVER (PARTITION BY "Genre") AS sum_gross_by_genre
+SELECT "Movie Name"
+FROM cte
+WHERE first_movie = "IMDB Rating";
+
+-- 4. Різниця між поточним і наступним рейтингом IMDB у межах жанру
+WITH cte AS (
+    SELECT *,
+           LEAD("IMDB Rating") OVER (PARTITION BY "Genre") AS next_rating
+    FROM movies
+)
+SELECT "Movie Name",
+       "IMDB Rating" - next_rating AS diff_rating
 FROM cte;
 
--- 5. Середня оцінка Metascore для кожного режисера
-SELECT *,
-       ROUND(AVG("Metascore") OVER (PARTITION BY "Director"), 2) AS avg_metascore_by_director
-FROM movies
-WHERE "Metascore" IS NOT NULL;
-
--- 6. Фільм із мінімальною тривалістю в кожному жанрі
+-- 5. Різниця голосів між поточним і наступним фільмом у межах року
 WITH cte AS (
-    SELECT * ,
-           MIN("Duration") OVER (PARTITION BY "Genre") AS min_duration_by_genre
+    SELECT *,
+           LAG(CAST(REPLACE("Votes", ',', '') AS INTEGER)) OVER (PARTITION BY "Release Year") AS next_votes_int
     FROM movies
 )
 SELECT "Movie Name",
-       "Duration"
-FROM cte
-WHERE min_duration_by_genre = "Duration";
+       CAST(REPLACE("Votes", ',', '') AS INTEGER) - next_votes_int AS diff_votes_lag
+FROM cte;
 
--- 7. Зростання кількості голосів у межах року
-WITH cte AS (
-    SELECT *,
-           CAST(REPLACE("Votes", ',', '') AS INTEGER) AS votes_int
-    FROM movies
-)
-SELECT *,
-       lag_votes - votes_int AS diff_votes
-FROM (
-    SELECT *,
-           LAG(votes_int) OVER (PARTITION BY "Release Year") AS lag_votes
-    FROM cte
-);
-
--- 8. Рейтинг IMDB у межах режисера
-WITH cte AS (
-    SELECT *,
-           MAX("IMDB Rating") OVER (PARTITION BY "Director") AS max_director_rating
-    FROM movies
-)
-SELECT "Director",
-       "Movie Name",
-       "IMDB Rating"
-FROM cte
-WHERE "IMDB Rating" = max_director_rating;
-
--- 9. Найбільш прибутковий жанр по роках
+-- 6. Пошук першого і останнього значення касових зборів у межах жанру
 WITH cte AS (
     SELECT *,
            CAST(REPLACE(REPLACE(REPLACE("Gross", '$', ''), 'M', '0'), '.', '') AS INTEGER) * 1000 AS gross_int
     FROM movies
     WHERE "Gross" != 'NA'
+), cte1 AS (
+    SELECT *,
+           FIRST_VALUE(gross_int) OVER (PARTITION BY "Genre") AS first_movie,
+           LAST_VALUE(gross_int) OVER (PARTITION BY "Genre") AS last_movie
+    FROM cte
 )
-SELECT "Release Year",
-       "Genre",
-       gross_int,
-       MAX(gross_int) OVER (PARTITION BY "Release Year", "Genre") AS max_gross_by_year_genre
-FROM cte;
+SELECT "Movie Name"
+FROM cte1
+WHERE gross_int IN (first_movie, last_movie);
+
+-- 7. Пошук максимального приросту касових зборів між фільмами в межах жанру
+WITH cte AS (
+    SELECT *,
+           CAST(REPLACE(REPLACE(REPLACE("Gross", '$', ''), 'M', '0'), '.', '') AS INTEGER) * 1000 AS gross_int
+    FROM movies
+    WHERE "Gross" != 'NA'
+), cte1 AS (
+    SELECT *,
+           LAG(gross_int) OVER (PARTITION BY "Genre") AS lag_gross
+    FROM cte
+), cte2 AS (
+    SELECT *,
+           gross_int - lag_gross AS diff_gros,
+           MAX(gross_int - lag_gross) OVER (PARTITION BY "Genre") AS max_diff_gross
+    FROM cte1
+)
+SELECT "Movie Name"
+FROM cte2
+WHERE diff_gros = max_diff_gross;
+
+-- 8. Різниця рейтингу IMDB між поточним і першим фільмом у межах режисера
+SELECT *,
+       "IMDB Rating" - first_value_by_director AS diff_rating
+FROM (
+    SELECT *,
+           FIRST_VALUE("IMDB Rating") OVER (PARTITION BY "Director") AS first_value_by_director
+    FROM movies
+);
+
+-- 9. Різниця рейтингу IMDB між поточним і останнім фільмом у межах року
+SELECT *,
+       "IMDB Rating" - last_value_by_year AS diff_rating
+FROM (
+    SELECT *,
+           LAST_VALUE("IMDB Rating") OVER (PARTITION BY "Release Year") AS last_value_by_year
+    FROM movies
+);
+
 
